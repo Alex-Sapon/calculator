a009c97258198d89c0efd0ec21bc2cd777d875bd
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-10-13 20:04:49 +0300

    initial commit

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path = require(`path`);
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    }
+  },
+};
